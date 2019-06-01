3cfc93e3230168d769382fb8128f5eeb33532f88
Ataris <512056026@qq.com>
2019-06-01 21:49:17 +0800

    修正错误答案
    直接用indexOf即可查找到

--- packages/1-two-sum/src/index.ts
+++ packages/1-two-sum/src/index.ts
@@ -6,8 +6,8 @@
 var twoSum = (nums: number[], target: number): number[] => {
     let index = 0;
     let answer = -1;
-    for (; index < nums.length; index++) {
-        answer = nums.slice(index).indexOf(target - nums[index]);
+    for (; index <= nums.length; index++) {
+        answer = nums.indexOf(target - nums[index], index + 1);
         if (answer !== -1) {
             break;
         }
