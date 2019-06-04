03a769098e9f0990921185a1fd12a7f380bf50a2
Ataris <ataris@sina.com>
2019-06-04 10:03:36 +0800

    第一次提交

--- packages/4-median-of-two-sorted-arrays/src/index.ts
+++ packages/4-median-of-two-sorted-arrays/src/index.ts
@@ -4,5 +4,26 @@
  * @return {number}
  */
 const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
-    
+    if (nums1.length === 0 || nums2.length === 0) {
+        const oneNums = nums1.length === 0 ? nums2 : nums1;
+        const length = oneNums.length;
+        if (length % 2 === 1) {
+            return oneNums[(length - 1) / 2];
+        } else {
+            return (oneNums[length / 2 - 1] + oneNums[length / 2]) / 2;
+        }
+    }
+    if (nums1.length === nums2.length && nums1.length === 1) {
+        return (nums1[0] + nums2[0]) / 2;
+    }
+    if (nums1[0] <= nums2[0]) {
+        if (nums1[nums1.length - 1] >= nums2[nums2.length - 1]) {
+            return findMedianSortedArrays(nums1.slice(1, nums1.length - 1), nums2);
+        }
+        return findMedianSortedArrays(nums1.slice(1), nums2.slice(0, nums2.length - 1));
+    }
+    if (nums1[nums1.length - 1] >= nums2[nums2.length - 1]) {
+        return findMedianSortedArrays(nums1.slice(0, nums1.length - 1), nums2.slice(1));
+    }
+    return findMedianSortedArrays(nums1, nums2.slice(1, nums2.length - 1));
 };
