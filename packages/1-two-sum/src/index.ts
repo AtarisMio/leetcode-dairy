9028db31f4ddebfab528fa106f678cda72e08d3e
Ataris <512056026@qq.com>
2020-03-09 23:33:06 +0800

    重做4题

--- packages/1-two-sum/src/index.ts
+++ packages/1-two-sum/src/index.ts
@@ -3,15 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const twoSum = (nums: number[], target: number): number[] => {
-    let hashmap: { [key: number]: number } = {};
-    for (let index = 0; index < nums.length; index++) {
-        let n = nums[index];
-        let test = target - n;
-        if (hashmap.hasOwnProperty(test)) {
-            return [hashmap[test], index];
-        }
-        hashmap[n] = index;
+  const hashmap: { [key: number]: number } = {};
+  for (let index = 0; index < nums.length; index += 1) {
+    const n = nums[index];
+    const test = target - n;
+    if (test in hashmap) {
+      return [hashmap[test], index];
     }
-    return [];
+    hashmap[n] = index;
+  }
+  return [];
 };
--- packages/4-median-of-two-sorted-arrays/src/index.ts
+++ packages/4-median-of-two-sorted-arrays/src/index.ts
@@ -3,27 +3,39 @@
  * @param {number[]} nums2
  * @return {number}
  */
-const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
-    if (nums1.length === 0 || nums2.length === 0) {
-        const oneNums = nums1.length === 0 ? nums2 : nums1;
-        const length = oneNums.length;
-        if (length % 2 === 1) {
-            return oneNums[(length - 1) / 2];
-        } else {
-            return (oneNums[length / 2 - 1] + oneNums[length / 2]) / 2;
-        }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, func-names
+const findMedianSortedArrays = function(
+  nums1: number[],
+  nums2: number[],
+): number {
+  // eslint-disable-next-line no-param-reassign
+  if (nums1.length > nums2.length) [nums1, nums2] = [nums2, nums1];
+
+  const length1 = nums1.length;
+  const length2 = nums2.length;
+  let min = 0;
+  let max = length1;
+  const half = Math.floor((length1 + length2 + 1) / 2);
+  while (max >= min) {
+    const i = Math.floor((max + min) / 2);
+    const j = half - i;
+    if (i > min && nums1[i - 1] > nums2[j]) {
+      max = i - 1;
+    } else if (i < max && nums1[i] < nums2[j - 1]) {
+      min = i + 1;
+    } else {
+      let left;
+      let right;
+      if (i === 0) left = nums2[j - 1];
+      else if (j === 0) left = nums1[i - 1];
+      else left = Math.max(nums1[i - 1], nums2[j - 1]);
+
+      if (i === length1) right = nums2[j];
+      else if (j === length2) right = nums1[i];
+      else right = Math.min(nums1[i], nums2[j]);
+
+      return (length1 + length2) % 2 ? left : (left + right) / 2;
     }
-    if (nums1.length === nums2.length && nums1.length === 1) {
-        return (nums1[0] + nums2[0]) / 2;
-    }
-    if (nums1[0] <= nums2[0]) {
-        if (nums1[nums1.length - 1] >= nums2[nums2.length - 1]) {
-            return findMedianSortedArrays(nums1.slice(1, nums1.length - 1), nums2);
-        }
-        return findMedianSortedArrays(nums1.slice(1), nums2.slice(0, nums2.length - 1));
-    }
-    if (nums1[nums1.length - 1] >= nums2[nums2.length - 1]) {
-        return findMedianSortedArrays(nums1.slice(0, nums1.length - 1), nums2.slice(1));
-    }
-    return findMedianSortedArrays(nums1, nums2.slice(1, nums2.length - 1));
+  }
+  return 0;
 };
